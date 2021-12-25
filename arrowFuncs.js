056636e99e79579cbf107347e30044a72a4c7074
Taylor Noj <taylornoj@gmail.com>
2021-12-25 21:35:17 +0000

    input info and examples of arrow functions

---
+++ arrowFuncs.js
@@ -0,0 +1,16 @@
+// ARROW FUNCTIONS //
+
+// better than function expressions
+// syntax:
+let func = (arg1, arg2, argN) => expression;
+//---------------//
+let sum = (a, b) => a + b;
+
+/* This arrow function is a shorter form of:
+
+let sum = function(a, b) {
+  return a + b;
+};
+*/
+
+alert( sum(1, 2) ); // 3
