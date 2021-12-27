874a18879b27796532b9466ae44b4aa6c2ba3324
Taylor Noj <taylornoj@gmail.com>
2021-12-27 17:35:50 +0000

    practice with methods in arrays

--- dataTypes/arrayTesting.js
+++ dataTypes/arrayTesting.js
@@ -1,5 +1,97 @@
 let arr = ["t", "e", "s", "t"];
+console.log(arr.slice(1, 3)); // e,s (copy from 1 to 3)
+console.log(arr.slice(-2)); // s,t (copy from -2 till the end)
 
-console.log( arr.slice(1, 3) ); // e,s (copy from 1 to 3)
+//------------------------------
 
-console.log( arr.slice(-2) ); // s,t (copy from -2 till the end)
+// write a function camelize(str) that changes dash separated words
+// like "my-short-string" into camel cased "myShortString"
+
+function camelize(str) {
+  return str
+    .split('-') // splits 'my-long-word' into array ['my', 'long', 'word']
+    .map(
+      // capitalizes first letters of all array items except the first one
+      // converts ['my', 'long', 'word'] into ['my', 'Long', 'Word']
+      (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .join(''); // joins ['my', 'Long', 'Word'] into 'myLongWord'
+}
+
+console.log(camelize("background-color"));
+
+//-------------------------------
+
+// sort in decreasing order
+
+let arr = [5, 2, 1, -10, 8];
+arr.sort((a, b) => b - a);
+console.log(arr);
+
+//-------------------------------
+
+// we have an array of strings and we'd like to have a sorted copy of
+// if, but keep arr unmodified
+
+function copySorted(arr) {
+  return arr.slice().sort();
+}
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+console.log( sorted );
+console.log( arr );
+
+//------------------------------
+
+// you have an array of user objects, each one has user.name
+// write code that converts it into an array of names
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let users = [ john, pete, mary ];
+
+let names = users.map(item => item.name);
+
+console.log( names ); // John, Pete, Mary
+
+//-------------------------------
+
+// write a function sortByAge(users) that gets an array of objects with the
+// age property and sorts them by age
+
+function sortByAge(arr) {
+  arr.sort((a, b) => a.age - b.age);
+}
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let arr = [ pete, john, mary ];
+
+sortByAge(arr);
+
+// now: [john, mary, pete]
+console.log(arr[0].name); // John
+console.log(arr[1].name); // Mary
+console.log(arr[2].name); // Pete
+
+// //--------------------------------
+
+// write the function getAverageAge(users) that gets an array of objects
+// with property age and returns the average age
+
+function getAverageAge(users) {
+  return users.reduce((prev, user) => prev + user.age, 0) / users.length;
+}
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 29 };
+
+let arr = [ john, pete, mary ];
+
+console.log( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
