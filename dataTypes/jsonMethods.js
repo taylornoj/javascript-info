4f2d6fe317ab1902227060742da7a796cacab4cd
Taylor Noj <taylornoj@gmail.com>
2021-12-28 17:18:06 +0000

    add summary

--- dataTypes/jsonMethods.js
+++ dataTypes/jsonMethods.js
@@ -183,3 +183,18 @@ alert(JSON.stringify(user, null, 2));
     }
 }
 */
+
+
+
+// 
+
+// JSON is a data format that has its own independent standard
+// and libraries for most programming languages
+
+// JSON supports plain objects, arrays, strings, numbers, booleans, and null
+
+// JavaScript provides methods JSON.stringify to serialize into JSON and
+// JSON.parse to read from JSON
+// both methods support transformer functions for smart reading/writing
+
+// if an object has toJSON then it is called by JSON.stringify
