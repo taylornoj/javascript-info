108d5b7014b4c3dcefee191a5d9263e39d8db626
Taylor Noj <taylornoj@gmail.com>
2021-12-28 22:10:53 +0000

    add new folder for advanced funcs and starting recursion

---
+++ advancedFuncs/recursion.js
@@ -0,0 +1,5 @@
+//// RECURSION AND STACK //// 
+
+// when a function solves a task, in the process it can call many other
+// functions. A partial case of this is when a function calls itself, that is
+// called recursion
