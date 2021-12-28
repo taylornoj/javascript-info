d76c30fb09becedf7e718eab919acf2ef1a70439
Taylor Noj <taylornoj@gmail.com>
2021-12-28 03:14:47 +0000

    add new file for object methods

---
+++ dataTypes/objectMethods.js
@@ -0,0 +1,7 @@
+//// OBJECT.KEYS, VALUES, ENTRIES ////
+
+// for plain objects, the following methods are available:
+Object.keys(obj)    // returns an array of keys
+Object.values(obj)  // returns an array of values
+Object.entries(obj) // returns an array of [key, value] pairs
+
