0ea601b749c572fdbb0bcaa1409b41110b29bf92
Taylor Noj <taylornoj@gmail.com>
2021-12-29 03:10:01 +0000

    add further recursion explanation

--- advancedFuncs/recursion.js
+++ advancedFuncs/recursion.js
@@ -29,3 +29,27 @@ function pow(x, n) {
   }
 }
 alert( pow(2, 3) ); // 8
+
+// when pow(x, n) is called, the execution splits into two branches:
+
+            if n==1  = x
+            /
+pow(x, n) =
+            \
+            else     = x * pow(x, n - 1)
+
+// if n == 1; everything is trivial - it is called the base of recursion because
+// it immediately produces the obvious result: pow(x, 1) == 1
+
+// otherwise, we can represent pow(x, n) as x * pow(x, n - 1)
+// this is the recursive step: we transform the task into a simpler action
+
+/*
+    To calculate pow(2, 4) the recursive variant does these steps:
+    
+    pow(2, 4) = 2 * pow(2, 3)
+    pow(2, 3) = 2 * pow(2, 2)
+    pow(2, 2) = 2 * pow(2, 1)
+    pow(2, 1) = 2
+
+*/
