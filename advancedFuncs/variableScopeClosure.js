962652ee4a7bdb475e8414a01d371d32eaf9b5ad
Taylor Noj <taylornoj@gmail.com>
2022-01-02 20:55:11 +0000

    add info regarding closures

--- advancedFuncs/variableScopeClosure.js
+++ advancedFuncs/variableScopeClosure.js
@@ -30,3 +30,51 @@ alert(i); // Error, no such variable
 
 // 'let i' looks like its outside of {...} but the 'for' construct is special, the variable,
 // declared insdie of it, is considered a part of the block
+
+//// CLOSURE
+/* a closure is a function that remembers its outer variables
+and can access them
+
+they automatically remember where they were created using a hidden
+[[Environment]] property & then their code can access outer variables
+
+
+
+** Lexical Environments
+When a function runs, at the beginning of the call, a new Lexical Environment
+is created automatically to store local variables and parameters of the call
+
+There are inner and outer Lexical Environments:
+*/
+let phrase = "Hello";
+function say(name) {
+  alert(`${phrase}, ${name}`);
+}
+say("John"); // Hello John
+/*
+The inner Lexical Environment corresponds to the current execution 
+of say. It has a single property: name, the function argument. 
+We called say("John"), so the value of the name is "John"
+
+The outer Lexical Environment is the global Lexical Environment. 
+It has the phrase variable and the function itself.
+
+Usually a Lexical Environment is removed from memory with all
+the variables after the function call finishes. Thats because there are 
+no references to it
+HOWEVER if there is a nested function that is still reachable after the end
+of a function, then it has [[Environment]] property that references
+the lexical environment
+
+Example:
+*/
+function f() {
+  let value = 123;
+
+  return function() {
+    alert(value);
+  }
+}
+let g = f(); // g.[[Environment]] stores a reference to the Lexical Environment
+// of the corresponding f() call
+
