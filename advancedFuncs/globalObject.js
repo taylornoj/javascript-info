9e97b1ac87508d147bd02ecdeaea64918617b12f
Taylor Noj <taylornoj@gmail.com>
2022-01-02 22:04:49 +0000

    quick note on global object added

---
+++ advancedFuncs/globalObject.js
@@ -0,0 +1,12 @@
+//// GLOBAL OBJECT ////
+
+// global object provides variables and functions that are available anywhere
+// in a browser it is named window
+
+// all properties are of the global object can be accessed directly:
+alert("Hello");
+// is the same as
+window.alert("Hello");
+
+// but global functions and variables are declared with var, not let/const,
+// so global variables are generally discouraged
